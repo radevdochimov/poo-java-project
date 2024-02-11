402f138ee9ab3d2ab1de2a0308ae5042103e0a60
Radames <evdochimov.radames@gmail.com>
2024-02-11 20:38:38 +0200

    Creating getName method

--- src/myproject/task2/Student.java
+++ src/myproject/task2/Student.java
@@ -14,9 +14,9 @@ public class Student {
     public Student() {
     }
 
-//    public String getName() {
-//        return name;
-//    }
+    public String getName() {
+        return name;
+    }
 //
 //    public void setName(String name) {
 //        this.name = name;
